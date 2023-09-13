29df7b51b5357f523d6bf0d26206103deb124241
ralfhartmann <ralfhartmann@gmx.net>
2023-09-13 20:28:07 +0200

    Update login.tsx

--- app/components/login.tsx
+++ app/components/login.tsx
@@ -1,9 +1,18 @@
 import { useOutletContext } from "@remix-run/react"
-
+import { useState } from 'react'
+    
 import type { SupabaseOutletContext } from "~/root"
 
 const Login = () => {
     const { supabase } = useOutletContext<SupabaseOutletContext>()
+    const [lastLoginEvent, setLastLoginEvent] = useState('<unkown>')
+    
+    supabase
+        .auth
+        .onAuthStateChange((event, session) => {
+            console.log(event, session)
+            setLastLoginEvent(event)
+    })
 
     const handleLogin = async () => {
         const { error } = await supabase.auth.signInWithOAuth({
@@ -25,6 +34,7 @@ const Login = () => {
 
     return (
         <>
+            <p>Last login event: {event}</p>
             <button onClick={handleLogout}>Logout</button>
             <button onClick={handleLogin}>Login</button>
         </>
